d55c60c1900b3c21f31b80e28f638a7a00367ab8
Gianluca Barranca <info@gianlucabarranca.it>
2024-05-30 21:50:59 +0200

    WIP: calculateScore

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const allSquare = document.querySelectorAll('.square')
-
-var go = 'circle'
+var go = 'cross'
 
 allSquare.forEach((val, key) => {
+    //  val.textContent = key //debug
     val.setAttribute('id', key)
     val.addEventListener('click', drawXO)
 })
@@ -13,4 +13,24 @@ function drawXO(e) {
     sign.classList.add(go)
     e.target.appendChild(sign)
     e.target.removeEventListener('click', drawXO)
+    calculateScore()
+}
+
+function calculateScore() {
+    const solutions = [
+        ["0", "1", "2"], ["3", "4", "5"], ["6", "7", "8"],
+        ["0", "3", "6"], ["1", "4", "7"], ["2", "5", "8"],
+        ["0", "4", "8"], ["2", "4", "6"]
+    ]
+
+    var winner = ""
+    var allCircles = document.querySelectorAll('.circle')
+    var allCrosses = document.querySelectorAll('.cross')
+
+    solutions.forEach((arr) => {
+        //prendo un array alla volta e controllo se i suoi elementi singoli sono tutti uguali agli id dentro allCircles
+        //   allCircles[0].parentNode.id
+    })
+
+    //  console.log(allCircles)
 }
